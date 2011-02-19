e2c16a31797f85393b210220b23504e2b68133ae
Derferman <kyle.j.conroy@gmail.com>
2011-02-18 17:35:05 -0800

    Added sphinxtogithub extension

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,10 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinxtogithub',
+]
 
 # Load the source for autodoc
 sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), '..')))
